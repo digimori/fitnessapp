c8058c0b50089b12b2eae1fed0b98d2a0a2a76ed
digimori <hanaorikuu@gmail.com>
2023-07-31 13:41:41 +0000

    feat: API hookup initiated. Testing secret key.

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,19 @@
 import { useEffect, useState } from "react";
 import React from 'react';
 import { Box, Button, Stack, TextField, Typography } from '@mui/material';
+import { exerciseOptions, fetchData } from "../utils/fetchData";
 
 const SearchExercises = () => {
+
+  const [search, setSearch] = useState('')
+
+  const handleSearch = async () => {
+    if(search) {
+      const exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises/bodyPartList', exerciseOptions)
+      console.log(exercisesData)
+    }
+  }
+
   return (
     <Stack alignItems="center"
            mt="37px"
@@ -19,14 +30,27 @@ const SearchExercises = () => {
             mb="72px">
             <TextField sx={{
               input: { fontWeight: '700', border:'none', borderRadius: '4px'},
-              width: { lg: '1170px', xs: '350px'},
+              width: { lg: '800px', xs: '350px'},
               backgroundColor: '#fff', borderRadius: '40px'
             }}
               height="76px"
-              value=""
-              onChange={() => {}}
+              value="Search"
+              onChange={(e) => {setSearch(e.target.value.toLowerCase())}}
               placeholder="Search Exercises"
               type="text" />
+
+              <Button className="search-btn"
+                      sx={{
+                        bgcolor: '#FF2625',
+                        color: '#fff',
+                        textTransform: 'none',
+                        width: { lg: '175px', xs: '80px' },
+                        fontSize: { lg: '20px', xs: '14px'},
+                        height: '56px',
+                        position: "absolute",
+                        right: '0'
+                      }}
+                      onClick={handleSearch}>Search</Button>
 
             </Box>
     </Stack>
