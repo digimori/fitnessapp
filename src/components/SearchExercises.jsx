5b69ded30abb875c95cfe0d32eb428851ff93c46
digimori <hanaorikuu@gmail.com>
2023-07-31 12:35:32 +0000

    feat: Search bar implemented for searching exercises. Not yet hooked up to API

--- src/components/SearchExercises.jsx
+++ src/components/SearchExercises.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import React from 'react';
+import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 const SearchExercises = () => {
   return (
-    <div>SearchExercises</div>
+    <Stack alignItems="center"
+           mt="37px"
+           justifyContent="center"
+           p="20px">
+            <Typography fontWeight={700}
+                        sx={{ fontSize: { lg: '44px', xs: '30px'}}}
+                        mb="50px"
+                        textAlign="center">
+              Awesome exercises you <br /> should know
+            </Typography>
+            <Box 
+            position="relative"
+            mb="72px">
+            <TextField sx={{
+              input: { fontWeight: '700', border:'none', borderRadius: '4px'},
+              width: { lg: '1170px', xs: '350px'},
+              backgroundColor: '#fff', borderRadius: '40px'
+            }}
+              height="76px"
+              value=""
+              onChange={() => {}}
+              placeholder="Search Exercises"
+              type="text" />
+
+            </Box>
+    </Stack>
   )
 }
 
