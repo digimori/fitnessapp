08b0cb1f9cec8806ecbbeb601daff421494c2d92
digimori <hanaorikuu@gmail.com>
2023-08-05 20:15:05 +0000

    Changed ENV variable - Prefaced with React_app as it's supposed to be

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,24 +1,22 @@
 export const exerciseOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-      'X-RapidAPI-Key': process.env.RAPID_API_SECRET_KEY,
-    },
-  };
-  
- export const youtubeOptions = {
-    method: 'GET',
-    url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
-    headers: {
-      'X-RapidAPI-Key': process.env.RAPID_API_SECRET_KEY,
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-    }
-  };
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_SECRET_KEY,
+  },
+};
 
-  
-  export const fetchData = async (url, options) => {
-    const res = await fetch(url, options);
-    const data = await res.json();
-  
-    return data;
-  };
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_SECRET_KEY,
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
